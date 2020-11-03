58a0f4a820a81fe244f7b2be1591c3cff8c89829
Amr Fouda <afouda@ibs-b.hu>
2020-11-03 15:14:05 +0100

    added

---
+++ week2/day3/Variable_mutation.py
@@ -0,0 +1,42 @@
+a = 3
+a +=10
+print(a)
+
+b = 100
+b -= 7
+print(b)
+
+c = 44
+c *= 2
+print(c)
+
+d = 125
+d /= 5
+print(d)
+
+e = 8
+e **= 2
+print(e)
+
+f1 = 123
+f2 = 345
+print(f1>f2)
+
+g1 = 350
+g2 = 200
+g2 *= 2
+print(g2>g1)
+
+h = 1357988018575474
+print(h % 11)
+
+i1 = 10
+i2 = 3
+i3 = 3
+i2 **= 2
+i3 **= 3
+print(i3<i1>i2)
+
+j = 1521
+if j %3 == 0 and j %5 == 0
+print("divisible by 3 and 5")
