98369e1f07c31a56b10103a4469cfcdc1c31d5ed
Amr Fouda <afouda@ibs-b.hu>
2020-11-22 16:11:15 +0100

    added

--- week9/Home assignment/home assignment.py
+++ week9/Home assignment/home assignment.py
@@ -1,3 +1,24 @@
 val = input("Command Line Todo application\n ==================\n-l List all the tasks\n-a Adds a new task\n-r Removes a task\n-c Completes a task\n")
 
-print ("user chose " + val)
+if val == "l":
+    fr = open('mylist.txt','r')
+    test = fr.read()
+    print (test)
+    fr.close()
+
+elif val == "a":
+    userIn = input("please enter your task item\n")
+    fr = open('mylist.txt', 'a')
+    fr.write("\n" + userIn)
+    fr.close()
+
+
+
+
+
+
+
+
+
+
+
